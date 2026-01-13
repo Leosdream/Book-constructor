b71cb8793ee1f8df3e8dc697e0048b50469d1ac1
Leosdream <133426388+Leosdream@users.noreply.github.com>
2026-01-13 22:15:11 +0200

    First plan

--- script.js
+++ script.js
@@ -13,3 +13,11 @@ function addBookToLibrary(title,author,pages,read) {
 const addBook=new Book(title,author,pages,read);
 myLibrary.push(addBook);
 }
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+
+function displayBooks(){
+for(let i=0; i<myLibrary.length();i++){
+//what im going to do is add a div suing the DOM and in it its going to be like a box with multiple other text in it. The big div will be in the container and its going to be display:grid
+}
+}
